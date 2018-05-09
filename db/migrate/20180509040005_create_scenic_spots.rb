038346c8bfafbba17c59c6f73a009317fe291208
VenseChang <sq851023@gmail.com>
2018-05-09 17:02:32 +0800

    change column name

--- db/migrate/20180509040005_create_scenic_spots.rb
+++ db/migrate/20180509040005_create_scenic_spots.rb
@@ -2,15 +2,15 @@ class CreateScenicSpots < ActiveRecord::Migration[5.2]
   def change
     create_table :scenic_spots do |t|
       t.string :name
-      t.text :DescriptionDetail
-      t.string :Description
+      t.text :description_detail
+      t.string :description
       t.string :phone
       t.string :address
-      t.string :OpenTime
+      t.string :open_time
       t.decimal :lat
       t.decimal :lng
       t.string :city
-      t.text :TravelInfo
+      t.text :travel_info
 
 
       t.timestamps
