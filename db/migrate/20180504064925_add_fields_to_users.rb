1ed14299fa154b6cde9ec06e2d9c34f6e3914525
VenseChang <sq851023@gmail.com>
2018-05-11 17:51:31 +0800

    fix type error

--- db/migrate/20180504064925_add_fields_to_users.rb
+++ db/migrate/20180504064925_add_fields_to_users.rb
@@ -4,7 +4,7 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :last_name, :string
     add_column :users, :nickname, :string
     add_column :users, :phone, :string
-    add_column :users, :avatar, :string渣ㄙ渣ㄙ
+    add_column :users, :avatar, :string
     add_column :users, :gender, :integer, default: 3
     add_column :users, :description, :text
     add_column :users, :device_type, :integer
