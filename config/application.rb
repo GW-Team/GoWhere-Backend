30f363a543cb2f3282ea7ace1e83d5382da64fed
Vense Chang <vense1023@gmail.com>
2018-10-28 12:19:58 +0800

    Add settings folder and store all env file

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,11 @@ module GoWhere
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'application.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      settings = Dir[Rails.root + 'config/settings/*.yml']
+      settings.each do |path|
+        puts path
+        YAML.load(File.open(path)).each{ |key, value| ENV[key.to_s] = value }
+      end
     end
 
 
